5b4ed7a01ee8dbfd2668ea197b083bbb32009a24
nir099 <a.m.s.nirmal.a@gmail.com>
2022-04-23 05:31:27 +0000

    ec2 setup complete

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -2,10 +2,14 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-    },
+	host: env("DATABASE_HOST"),
+     	 port: env.int("DATABASE_PORT"),
+     	 database: env("DATABASE_NAME"),
+     	 user: env("DATABASE_USERNAME"),
+     	 password: env("DATABASE_PASSWORD"),
+      },
     useNullAsDefault: true,
   },
 });
