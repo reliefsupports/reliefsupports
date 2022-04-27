a3257497eb1af49fa9037f6778feb8f6fbc10455
Harrish-Selvarajah <selvarajahharrish@gmail.com>
2022-04-27 22:55:26 +0530

    added api integration to request table

--- web/src/components/Requests/index.tsx
+++ web/src/components/Requests/index.tsx
@@ -1,55 +1,51 @@
+// import { getDocuments } from 'hooks/useGetDocuments';
+import { useGetDocuments } from 'hooks/useGetDocuments';
+import { useEffect } from 'react';
 import { Table } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 
 export default function Request() {
+  const { data, getDocuments } = useGetDocuments();
+  useEffect(() => {
+    getDocuments({ query: 'request' });
+  }, [data]);
+
   return (
-    <div style={{ padding: '20px' }}>
-      <div>
-        <Button>Add Requests</Button>
+    <>
+      <div style={{ padding: '20px' }}>
+        <div>
+          <Button>Add Requests</Button>
+        </div>
+        <br />
+        <Table striped bordered hover responsive>
+          <thead>
+            <tr>
+              <th>District</th>
+              <th>Description</th>
+              {/* <th>Tags</th> */}
+              <th>Prority</th>
+              <th>Category</th>
+              <th>Responses</th>
+              <th>Status</th>
+              {/* <th>Is Verified</th> */}
+              {/* <th>Posted By</th> */}
+              {/* <th>Logs</th> */}
+            </tr>
+          </thead>
+          <tbody>
+            {data?.map((d) => (
+              <tr>
+                <td>{d.attributes.district}</td>
+                {/* <td>{d.attributes.createdAt.toDateString()}</td> */}
+                <td>{d.attributes.description}</td>
+                <td>{d.attributes.priority}</td>
+                <td>{d.attributes.category}</td>
+                <td>{d.attributes.status}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
       </div>
-      <br />
-      <Table striped bordered hover responsive>
-        <thead>
-          <tr>
-            <th>District</th>
-            <th>Description</th>
-            <th>Tags</th>
-            <th>Prority</th>
-            <th>Category</th>
-            <th>Responses</th>
-            <th>Status</th>
-            <th>Is Verified</th>
-            <th>Posted By</th>
-            <th>Logs</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Colombo</td>
-            <td>Need Emergency Medicine</td>
-            <td>Medicine</td>
-            <td>High</td>
-            <td>Medicine</td>
-            <td>NOne</td>
-            <td>Active</td>
-            <td>Verified</td>
-            <td>Harrish</td>
-            <td>none</td>
-          </tr>
-          {/* <tr>
-            <td>2</td>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Larry the Bird</td>
-            <td>@twitter</td>
-            <td>@fat</td>
-          </tr> */}
-        </tbody>
-      </Table>
-    </div>
+    </>
   );
 }
