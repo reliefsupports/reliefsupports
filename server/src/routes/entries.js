dc9bfaab615fe42b29869bfb254a6558cdb81527
thinkholic <mailtokasun@gmail.com>
2022-05-14 22:30:43 +0530

    Add entries endpoint

--- server/src/routes/entries.js
+++ server/src/routes/entries.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const ShortUniqueId = require('short-unique-id');
+const pick = require('lodash/pick');
 
 const router = express.Router();
+const uid = new ShortUniqueId();
 
 const Entry = require('../modals/entry');
 
-// @todo: validate
+// @todo: [improvements] move handlers into separate files
+
+// ::entries.get
 router.get('/', async (req, res) => {
   try {
     const entries = await Entry.find({});
@@ -12,88 +17,117 @@ router.get('/', async (req, res) => {
       code: 200,
       message: entries,
     });
-  } catch (error) {
+  } catch (errors) {
+    // @todo: return proper error code
     return res.status(500).json({
       code: 500,
-      error: 'Internal Server Error!',
+      errors: errors,
     });
   }
 });
 
+// ::entries.getSingle
 router.get('/:id', async (req, res) => {
   try {
-    const entry = await Entry.findOne({ id: req.params.id });
+    const entry = await Entry.findOne({ id: req.params.id }); // @todo: allow for _id also
+
+    // @todo: handle no-data properly
+
     return res.status(200).json({
       code: 200,
       message: entry,
     });
-  } catch (error) {
+  } catch (errors) {
+    // @todo: return proper error code
     return res.status(500).json({
       code: 500,
-      error: 'Internal Server Error!',
+      errors: errors,
     });
   }
 });
 
+// ::entries.post
 router.post('/', async (req, res) => {
   try {
-    const body = req.body;
+    const body = pick(req.body, [
+      'type',
+      'category',
+      'summary',
+      'body',
+      'author',
+      'priority',
+      'location',
+      'externalSource',
+    ]);
+    // @todo: pre-validate values, and kick out if invalid.
 
-    const entry = new Entry(body);
+    const entry = new Entry({
+      id: uid(4),
+      ...body,
+    });
+
     await entry.save();
     return res.status(200).json({
       code: 200,
       message: entry,
     });
-  } catch (error) {
+  } catch (errors) {
+    // @todo: return proper error code
     return res.status(500).json({
       code: 500,
-      error: 'Internal Server Error!',
+      errors: errors, // @todo: parse these errors to human-readable text
     });
   }
 });
 
+// ::entries.patch-primary-fields
 router.patch('/:id', async (req, res) => {
   try {
-    const entry = await Entry.findOne({ _id: req.params.id });
+    const entry = await Entry.findOne({ id: req.params.id });
 
-    const { summary, body } = req.body;
+    const { summary, body, priority, status } = req.body;
+    // @todo: pre-validate values, and kick out if invalid.
 
-    if (summary) {
-      entry.summary = summary;
-    }
-
-    if (body) {
-      entry.description = body;
-    }
+    if (summary) entry.summary = summary;
+    if (body) entry.body = body;
+    if (priority) entry.priority = priority;
+    if (status) entry.status = status;
+    // @todo: add other fields as necessory
 
     await entry.save();
     return res.status(200).json({
       code: 200,
       message: entry,
     });
-  } catch (error) {
+  } catch (errors) {
+    // @todo: return proper error code
     return res.status(500).json({
       code: 500,
-      error: 'Internal Server Error!',
+      errors: errors,
     });
   }
 });
 
+// ::entries.delete
 router.delete('/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    await Entry.deleteOne({ _id: req.params.id });
+
+    // @todo: handle no-data properly
+
+    await Entry.deleteOne({ id: req.params.id });
+
     return res.status(200).json({
       code: 200,
       message: {
-        _id: id,
+        id: id,
       },
     });
-  } catch (error) {
+  } catch (errors) {
+    // @todo: return proper error code
     return res.status(500).json({
       code: 500,
-      error: 'Internal Server Error!',
+      errors: errors,
     });
   }
 });
