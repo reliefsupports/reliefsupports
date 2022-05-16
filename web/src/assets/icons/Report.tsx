8e4aa9d5f2abd88c1419091ecadc9d1265841526
T-JAY\Thiruni <Thiruni Jayasiriwardena>
2022-05-16 14:57:31 +0530

    Implementation

---
+++ web/src/assets/icons/Report.tsx
@@ -0,0 +1,30 @@
+export default function Report(props: any) {
+  return (
+    <>
+      <svg
+        width="10"
+        height="10"
+        viewBox="0 0 10 10"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M6.865 0.5H3.135L0.5 3.135V6.865L3.135 9.5H6.865L9.5 6.865V3.135L6.865 0.5ZM8.5 6.45L6.45 8.5H3.55L1.5 6.45V3.55L3.55 1.5H6.45L8.5 3.55V6.45V6.45Z"
+          fill="#A3A3A3"
+        />
+        <svg
+          width="2"
+          height="6"
+          viewBox="0 0 2 6"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0.5 0.5H1.5V3.5H0.5V0.5ZM0.5 4.5H1.5V5.5H0.5V4.5Z"
+            fill="#A3A3A3"
+          />
+        </svg>
+      </svg>
+    </>
+  );
+}
---
+++ web/src/utils/entries.ts
@@ -0,0 +1,32 @@
+import { IEntry } from '../types';
+
+export const getReqType = (locations: IEntry): any =>
+  locations ? locations.type : null;
+
+export const getReqPriority = (locations: IEntry): any =>
+  locations ? locations.priority : null;
+
+export const getAuthorName = (locations: IEntry): any =>
+  locations && locations.author.name ? locations.author.name : null;
+
+export const getCategory = (locations: IEntry): any =>
+  locations && locations.category ? locations.category : null;
+
+export const getDistrict = (locations: IEntry): any =>
+  locations && locations.location && locations.location.city
+    ? locations.location.city
+    : null;
+    
+export const getStatus = (locations: IEntry): any =>
+  locations && locations.status && locations.status ? locations.status : null;
+
+export const getSummary = (locations: IEntry): any =>
+  locations && locations.summary ? locations.summary : null;
+
+export const getOrganization = (locations: IEntry): any =>
+  locations && locations.author && locations.author.orgnization
+    ? locations.author.orgnization
+    : null;
+
+export const IsVerified = (locations: IEntry): any =>
+  locations && locations.isVerified ? locations.isVerified : false;
