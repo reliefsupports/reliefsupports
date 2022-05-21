b3afb8f3425a69ccf55a33e67936984bf69ece62
Wickramaranga Abeygunawardhana <w@umstek.tk>
2022-05-21 22:24:53 +0530

    Add comments view and editor components

--- web/src/screens/Entry/index.tsx
+++ web/src/screens/Entry/index.tsx
@@ -3,7 +3,7 @@ import dayjs from 'dayjs';
 
 import VerifiedIcon from 'assets/icons/Verified';
 
-import { IEntry } from 'types';
+import { IEntry, IComment } from 'types';
 import PageLayout from 'layouts/PageLayout';
 
 import {
@@ -15,6 +15,7 @@ import {
   getCategory,
   getSummary,
   getOrganization,
+  buildCommentsTree,
 } from 'utils/entries';
 
 import {
@@ -28,12 +29,135 @@ import {
   Body,
   Phone,
 } from './styled';
+import TextArea from '../../components/TextArea';
+import { Formik } from 'formik';
+import { Button } from '../../components/core';
+import { createComment } from '../../api/entries';
+import { useMemo, useState } from 'react';
+
+interface ICommentEditorProps {
+  postId: string;
+  replyingTo?: string;
+}
+
+const initialValues: any = {
+  body: '',
+};
+
+function CommentEditor({ postId, replyingTo }: ICommentEditorProps) {
+  return (
+    <Formik
+      initialValues={initialValues}
+      validate={(values) => {
+        const errors: any = {};
+        return errors;
+      }}
+      onSubmit={async (values, { setSubmitting, resetForm }) => {
+        setSubmitting(true);
+        values.body = '<p>'.concat(values.body, '</p>');
+        const response = await createComment(postId, {
+          ...values,
+          ...(replyingTo && { parent: replyingTo }),
+          ...{
+            author: {
+              name: 'Snoop dogg',
+              phone: '+94711111111',
+              avatarUrl: null,
+              orgnization: null,
+            },
+          },
+        });
+        if (response) {
+          setSubmitting(false);
+          resetForm();
+        }
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        setFieldValue,
+        /* and other goodies */
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <TextArea
+            name="body"
+            label="Comment"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.body}
+            error={errors.body}
+            touched={touched.body}
+          />
+          <div style={{ marginTop: 20 }}>
+            <Button type="submit" disabled={isSubmitting}>
+              Save
+            </Button>
+          </div>
+        </form>
+      )}
+    </Formik>
+  );
+}
+
+function Comment(comment: IComment & { postId: string }): JSX.Element {
+  const [showReply, setShowReply] = useState(false);
+
+  return (
+    <div
+      style={{
+        borderLeft: '1px solid #e6e6e6',
+      }}
+      id={`comment_${comment.id}`}
+    >
+      <Body dangerouslySetInnerHTML={{ __html: comment.body }} />
+      <Meta>
+        {dayjs(comment.createdAt).format('MMM DD, YYYY HH:mm')} &bull;{' '}
+        <span>
+          {` by ${getAuthorName(comment)}`}{' '}
+          {getOrganization(comment) && (
+            <span>{` on behalf of ${getOrganization(comment)}`} </span>
+          )}
+        </span>
+      </Meta>
+      <div style={{ paddingLeft: '1rem' }}>
+        <Button
+          onClick={() => setShowReply(!showReply)}
+          style={{
+            backgroundColor: '#f5f5f5',
+            border: '1px solid #f5f5f5',
+            color: '#000',
+          }}
+        >
+          {showReply ? 'Hide reply' : 'Reply'}
+        </Button>
+        {showReply && (
+          <CommentEditor postId={comment.postId} replyingTo={comment.id} />
+        )}
+        {comment?.children?.map((c) => (
+          <Comment key={`reply_${c.id}`} postId={comment.postId} {...c} />
+        ))}
+      </div>
+    </div>
+  );
+}
 
 export default function EntrySingle() {
   const location = useLocation();
 
   const state = location.state as IEntry;
   const { id, createdAt, isVerified } = state;
+
+  const commentsTree = useMemo(() => {
+    return buildCommentsTree(state.comments || []);
+  }, [state.comments]);
+
+  console.log(commentsTree);
 
   return (
     <PageLayout>
@@ -64,6 +188,10 @@ export default function EntrySingle() {
 
         <Phone>Phone: {state.author.phone}</Phone>
       </Conatiner>
+      {commentsTree.map((c) => (
+        <Comment key={`comment_${c.id}`} postId={state.id} {...c} />
+      ))}
+      <CommentEditor postId={id} />
     </PageLayout>
   );
 }
--- web/src/utils/entries.ts
+++ web/src/utils/entries.ts
@@ -1,4 +1,4 @@
-import { IEntry } from '../types';
+import { IComment, IEntry } from '../types';
 
 export const getReqType = (locations: IEntry): any =>
   locations ? locations.type : null;
@@ -6,7 +6,7 @@ export const getReqType = (locations: IEntry): any =>
 export const getReqPriority = (locations: IEntry): any =>
   locations ? locations.priority : null;
 
-export const getAuthorName = (locations: IEntry): any =>
+export const getAuthorName = (locations: IEntry | IComment): any =>
   locations && locations.author.name ? locations.author.name : null;
 
 export const getCategory = (locations: IEntry): any =>
@@ -23,10 +23,42 @@ export const getStatus = (locations: IEntry): any =>
 export const getSummary = (locations: IEntry): any =>
   locations && locations.summary ? locations.summary : null;
 
-export const getOrganization = (locations: IEntry): any =>
+export const getOrganization = (locations: IEntry | IComment): any =>
   locations && locations.author && locations.author.orgnization
     ? locations.author.orgnization
     : null;
 
 export const IsVerified = (locations: IEntry): any =>
   locations && locations.isVerified ? locations.isVerified : false;
+
+export const buildCommentsTree = (comments: IComment[]): IComment[] => {
+  const topLevel: IComment[] = [];
+
+  // Assume: comments are sorted by date, replies always come after their parent
+  const index: { [id: string]: IComment } = {};
+  for (const comment of comments) {
+    index[comment.id] = comment;
+    comment.children = [];
+
+    const { parent } = comment;
+    if (parent) {
+      // If it has a parent, it's a reply
+      if (!index[parent]) {
+        // Reply to a deleted comment
+        index[parent] = {
+          id: parent,
+          body: '<i>deleted</i>',
+          author: { name: '<i>deleted</i>', phone: '<i>deleted</i>' },
+          createdAt: comment.createdAt,
+          lastUpdatedAt: comment.lastUpdatedAt,
+          children: [],
+        };
+      }
+      index[parent].children.push(comment);
+    } else {
+      topLevel.push(comment);
+    }
+  }
+
+  return topLevel;
+};
